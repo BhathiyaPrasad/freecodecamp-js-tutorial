f96cd5247c9e5d8249a21e9a106e29c7d39d2673
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2023-12-29 17:13:23 +0530

    updated

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-document.getElementById("header").innerHTML = "this is footer";
+// document.getElementById("header").innerHTML = "this is footer";
+
+// let count = 8
+// console.log(count)
+
+let myAge = 21
+console.log(myAge)
