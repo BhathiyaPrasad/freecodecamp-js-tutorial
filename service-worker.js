5d72672ae7d566e961af68012446c0eb61dada65
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-01-12 09:55:57 +0530

    creating service worker script

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+// service-worker.js
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+      caches.open('my-cache').then(cache => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/styles.css',
+          '/script.js'
+          // Add other files or assets you want to cache
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', event => {
+    event.respondWith(
+      caches.match(event.request).then(response => {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
+  // Add other event listeners for push notifications, background sync, etc.
+  
